b30c7dbebe7f2ca8e300967429f2ca2fa6a37046
Amy Johnson and Tina Ramsey <amyjohnsonmichelle@gmail.com>
2015-03-16 14:26:02 -0700

    add methods to mark tasks as complete

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ActiveRecord::Base
   belongs_to :list
 
+  scope :complete, -> { where(done: true) }
+  scope :not_complete, -> { where(done: false) }
   validates :description, :presence => true
 end
